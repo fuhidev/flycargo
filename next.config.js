e110805eaa200388be431d9cdd5360fbbad68132
PhuongHieuHo <fuhi.dev@gmail.com>
2025-07-25 15:26:40 +0700

    Refactor code structure for improved readability and maintainability

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  typescript: {
-    // ignoreBuildErrors: true,
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+ output: "export",
+ images: {
+  unoptimized: true,
+ },
+ typescript: {
+  // ignoreBuildErrors: true,
+ },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
